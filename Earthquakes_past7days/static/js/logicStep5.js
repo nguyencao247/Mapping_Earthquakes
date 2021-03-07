91bcff1d2e5945c8d502cc734fe8618e5864140c
Nguyen Cao <nguyen.cao247@hotmail.com>
2021-03-07 15:46:12 -0500

    updating

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -65,6 +65,37 @@ earthquakes.addTo(map);
 
 });
 
+// Create a legend control object.
+let legend = L.control({
+  position: "bottomright"
+});
+
+// Then add all the details for the legend.
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div", "info legend");
+  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const colors = [
+    "#98ee00",
+    "#d4ee00",
+    "#eecc00",
+    "#ee9c00",
+    "#ea822c",
+    "#ea2c2c"
+  ];
+
+  // Looping through our intervals to generate a label with a colored square for each interval.
+  for (var i = 0; i < magnitudes.length; i++) {
+    console.log(colors[i]);
+    div.innerHTML +=
+      "<i style='background: " + colors[i] + "'></i> " +
+      magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+  }
+  return div;
+
+};
+
+legend.addTo(map);
+
 // This function returns the style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into a function
 // to calculate the radius.
